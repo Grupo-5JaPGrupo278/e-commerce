bda4bcddd6c875b1807487b27b512a234be3d5ba
recordeco <contacto.ivanpereira@gmail.com>
2023-10-17 14:50:00 -0300

    Popup Welcome

--- js/index.js
+++ js/index.js
@@ -22,3 +22,26 @@ button.addEventListener("click",function(e){
         this.classList.add("displayNone")
     }, 1000);
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const closeButton = document.getElementById("button-close");
+    const noMostrarCheckbox = document.getElementById("no-mostrar");
+
+    // Función para cerrar el popup
+    function closePopup() {
+        termsPopup.style.display = "none";
+
+        // Guardar la preferencia del usuario si marca "No volver a mostrar"
+        if (noMostrarCheckbox.checked) {
+            document.cookie = "mostrarPopup=0; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+        }
+    }
+
+    // Mostrar el popup si la cookie 'mostrarPopup' no está configurada
+    if (document.cookie.indexOf("mostrarPopup=0") === -1) {
+        termsPopup.style.display = "block";
+    }
+
+    // Cerrar el popup al hacer clic en el botón "Cerrar"
+    closeButton.addEventListener("click", closePopup);
+});
