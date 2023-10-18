c9cf1bae90b5bfaab741aa20bebfaccd1d902c51
Franco Echaide <echaidefranco@gmail.com>
2023-10-18 18:11:38 -0300

    Funcionalidad Costos

--- js/cart.js
+++ js/cart.js
@@ -1,40 +1,40 @@
-const CONTAINER = document.getElementById('cart-list');
-const INFO = JSON.parse(localStorage.getItem('cartlist'));
-const QUANTITIES = document.getElementsByClassName('input-quantity');
-// Función para borrar Items del Carrito
+const CONTAINER = document.getElementById("cart-list");
+const INFO = JSON.parse(localStorage.getItem("cartlist"));
+const QUANTITIES = document.getElementsByClassName("input-quantity");
 function DeleteCartItem(e) {
-    const ITEMID = Number(e.target.getAttribute('cart-id'))
-    const NEWLSCARTLIST = [];
-    for (let i = 0; i < INFO.length; i++) {
-        if (!(INFO[i].id === ITEMID)) {
-            NEWLSCARTLIST.push(INFO[i])
-        }
-    }
-    localStorage.setItem('cartlist', JSON.stringify(NEWLSCARTLIST));
-    location.reload();
+	const ITEMID = Number(e.target.getAttribute("cart-id"));
+	console.log(ITEMID);
+	const NEWLSCARTLIST = [];
+	for (let i = 0; i < INFO.length; i++) {
+		if (!(INFO[i].id === ITEMID)) {
+			NEWLSCARTLIST.push(INFO[i]);
+		}
+	}
+	console.log(NEWLSCARTLIST);
+	localStorage.setItem("cartlist", JSON.stringify(NEWLSCARTLIST));
+	location.reload();
 }
-// Función para cambiar las cantidades de los Items en el Carrito Y calcular el nuevo Subtotal
 function QuantityChange(e) {
-  // Agregar Item Quantity al LS y reimprimirlo continuamente
-    for (let i = 0; i < INFO.length; i++){
-      if ( INFO[i].id == e.target.id){
-        INFO[i].quantity = e.target.valueAsNumber;
-        localStorage.setItem('cartlist',JSON.stringify(INFO))
-      }
-    }
-    const quantity = e.target.valueAsNumber;
-    const row = e.target.parentElement.parentElement; // Obtén la fila actual
-    const cost = parseFloat(row.querySelector('.cost').textContent.replace(/\D/g, ''));
-    const subtotal = quantity * cost;
-    row.querySelector('.subtotal').textContent = `${INFO[row.rowIndex - 1].currency} ${subtotal}`;
+	// Agregar Item Quantity al LS y reimprimirlo continuamente
+	for (let i = 0; i < INFO.length; i++) {
+		if (INFO[i].id == e.target.id) {
+			INFO[i].quantity = e.target.valueAsNumber;
+			localStorage.setItem("cartlist", JSON.stringify(INFO));
+		}
+	}
+	const quantity = e.target.valueAsNumber;
+	const row = e.target.parentElement.parentElement; // Obtén la fila actual
+	const cost = parseFloat(row.querySelector(".cost").textContent.replace(/\D/g, ""));
+	const subtotal = quantity * cost;
+	row.querySelector(".subtotal").textContent = `${INFO[row.rowIndex - 1].currency} ${subtotal}`;
+	changeTotalCost();
 }
-// Impresora de artículos en el Carrito
 function ShowCart() {
-    let htmlContentToAppend = '';
-    let cartitemrows = '';
-    for (let i = 0; i < INFO.length; i++) {
-        cartitemrows += `
-        <tr>
+	let htmlContentToAppend = "";
+	let cartitemrows = "";
+	for (let i = 0; i < INFO.length; i++) {
+		cartitemrows += `
+        <tr class="cart">
               <td scope="row"><img class="little-cart-item" src="${INFO[i].imgsource}" alt="cart item"></td>
               <td>${INFO[i].name}</td>
               <td class="cost">${INFO[i].currency} ${INFO[i].cost}</td>
@@ -59,11 +59,11 @@ function ShowCart() {
                 >
               </td>
             </tr>
-        `
-    }
+        `;
+	}
 
-    if (cartitemrows === '') {
-        htmlContentToAppend = `
+	if (cartitemrows === "") {
+		htmlContentToAppend = `
     <table class="table">
           <thead>
             <tr>
@@ -78,9 +78,9 @@ function ShowCart() {
           </tbody>
         </table>
         <p>No se encontraron artículos agregados al carrito</p>
-    `
-    } else {
-        htmlContentToAppend = `
+    `;
+	} else {
+		htmlContentToAppend = `
     <table class="table">
           <thead>
             <tr>
@@ -95,11 +95,56 @@ function ShowCart() {
             ${cartitemrows}
           </tbody>
         </table>
-    `
-    }
+    `;
+	}
 
-
-    CONTAINER.innerHTML = htmlContentToAppend
-    
+	CONTAINER.innerHTML = htmlContentToAppend;
 }
-document.addEventListener('DOMContentLoaded', ShowCart);
+document.addEventListener("DOMContentLoaded", ShowCart);
+
+function changeTotalFinal() {
+	let t =
+		parseFloat(document.getElementById("CostoEnvio").textContent.replace(/\D/g, "")) +
+		parseFloat(document.getElementById("Total-Productos").textContent.replace(/\D/g, ""));
+	document.getElementById("Total").innerHTML = "U$D " + t;
+}
+
+function changeTotalCost() {
+	const cart = document.getElementsByClassName("subtotal");
+	let costTotal = 0;
+	for (let i = 0; i < cart.length; i++) {
+		costTotal += parseFloat(cart[i].textContent.replace(/\D/g, ""));
+	}
+
+	document.getElementById("Total-Productos").innerHTML = "U$D " + costTotal;
+	ChangeCostoEnvio();
+	changeTotalFinal();
+}
+
+function ChangeCostoEnvio() {
+	let totalC = parseFloat(document.getElementById("Total-Productos").textContent.replace(/\D/g, ""));
+	const CostoEnvio = document.getElementsByClassName("custom-control-input");
+	let inputEnvios;
+	for (let i = 0; i < CostoEnvio.length; i++) {
+		if (CostoEnvio[i].checked) {
+			inputEnvios = CostoEnvio[i];
+		}
+	}
+	if (inputEnvios.id === "goldradio") {
+		document.getElementById("CostoEnvio").innerHTML = "U$D " + Math.round(totalC * 0.13);
+	} else if (inputEnvios.id === "premiumradio") {
+		document.getElementById("CostoEnvio").innerHTML = "U$D " + Math.round(totalC * 0.07);
+	} else {
+		document.getElementById("CostoEnvio").innerHTML = "U$D " + Math.round(totalC * 0.03);
+	}
+
+	changeTotalFinal();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	changeTotalCost();
+
+	ChangeCostoEnvio();
+
+	changeTotalFinal();
+});
